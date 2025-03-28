a1fe28cba0b6460752f2da4259b33b4b9779d1a4
devvagnerBR <wagnerluizsg@outlook.com.br>
2025-03-28 11:08:23 -0300

    v3.0.1

--- src/app/licitati/components/licitati-client-side.tsx
+++ src/app/licitati/components/licitati-client-side.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { Suspense } from 'react'
 import { useState } from "react";
 import { FormFilters } from "./form-filters";
 import { ComprasProps } from "../types/compra";
@@ -14,15 +14,17 @@ export function LicitatiClientSide() {
   const [loading, setLoading] = useState<boolean>( false );
 
   return (
-    <div className="p-4 flex flex-col gap-4 mt-4">
-      <h1 className="text-18 text-neutral-800">Busca por compras</h1>
-      <FormFilters
-        setLoading={setLoading}
-        setCompras={setCompras} />
-      <ListCompras
-        loading={loading}
-        setCompras={setCompras}
-        compras={compras} />
-    </div>
+    <Suspense>
+      <div className="p-4 flex flex-col gap-4 mt-4">
+        <h1 className="text-18 text-neutral-800">Busca por compras</h1>
+        <FormFilters
+          setLoading={setLoading}
+          setCompras={setCompras} />
+        <ListCompras
+          loading={loading}
+          setCompras={setCompras}
+          compras={compras} />
+      </div>
+    </Suspense>
   );
 }
